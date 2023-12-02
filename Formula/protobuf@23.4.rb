d4d7af1b3e902e25adf849199989f9554a00bf77
Steve Nogar <stephen.m.nogar.civ@army.mil>
2023-12-02 12:23:16 -0500

    update formulas

---
+++ Formula/protobuf@23.4.rb
@@ -0,0 +1,90 @@
+class ProtobufAT234 < Formula
+  desc "Protocol buffers (Google's data interchange format)"
+  homepage "https://protobuf.dev/"
+  url "https://github.com/protocolbuffers/protobuf/releases/download/v23.4/protobuf-23.4.tar.gz"
+  sha256 "a700a49470d301f1190a487a923b5095bf60f08f4ae4cac9f5f7c36883d17971"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  head do
+    url "https://github.com/protocolbuffers/protobuf.git", branch: "main"
+    depends_on "jsoncpp"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "abseil"
+  # TODO: Add the dependency below in Protobuf 24+. Also remove `head` block.
+  # TODO: depends_on "jsoncpp"
+
+  uses_from_macos "zlib"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    odie "Dependencies need adjusting!" if build.stable? && version >= "24"
+    # Keep `CMAKE_CXX_STANDARD` in sync with the same variable in `abseil.rb`.
+    abseil_cxx_standard = 17
+    cmake_args = %w[
+      -DBUILD_SHARED_LIBS=ON
+      -Dprotobuf_BUILD_LIBPROTOC=ON
+      -Dprotobuf_BUILD_SHARED_LIBS=ON
+      -Dprotobuf_INSTALL_EXAMPLES=ON
+      -Dprotobuf_BUILD_TESTS=OFF
+      -Dprotobuf_ABSL_PROVIDER=package
+      -Dprotobuf_JSONCPP_PROVIDER=package
+    ]
+    cmake_args << "-DCMAKE_CXX_STANDARD=#{abseil_cxx_standard}"
+
+    system "cmake", "-S", ".", "-B", "build", *cmake_args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    (share/"vim/vimfiles/syntax").install "editors/proto.vim"
+    elisp.install "editors/protobuf-mode.el"
+
+    ENV.append_to_cflags "-I#{include}"
+    ENV.append_to_cflags "-L#{lib}"
+    ENV["PROTOC"] = bin/"protoc"
+
+    cd "python" do
+      # Keep C++ standard in sync with `abseil.rb`.
+      inreplace "setup.py", "extra_compile_args.append('-std=c++14')",
+                            "extra_compile_args.append('-std=c++#{abseil_cxx_standard}')"
+      pythons.each do |python|
+        pyext_dir = prefix/Language::Python.site_packages(python)/"google/protobuf/pyext"
+        with_env(LDFLAGS: "-Wl,-rpath,#{rpath(source: pyext_dir)} #{ENV.ldflags}".strip) do
+          system python, *Language::Python.setup_install_args(prefix, python), "--cpp_implementation"
+        end
+      end
+    end
+  end
+
+  test do
+    testdata = <<~EOS
+      syntax = "proto3";
+      package test;
+      message TestCase {
+        string name = 4;
+      }
+      message Test {
+        repeated TestCase case = 1;
+      }
+    EOS
+    (testpath/"test.proto").write testdata
+    system bin/"protoc", "test.proto", "--cpp_out=."
+
+    pythons.each do |python|
+      system python, "-c", "from google.protobuf.pyext import _message"
+    end
+  end
+end
